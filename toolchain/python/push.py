4e00f1b5c7a14ff8186d75f1942811c2f31bbd08
vsdum <danmarkhunter13072006@gmail.com>
2021-10-22 22:59:36 +0300

    ADB push fix

--- toolchain/python/push.py
+++ toolchain/python/push.py
@@ -26,10 +26,9 @@ def get_push_pack_directory():
 
 
 def push(directory, cleanup=False):
-	items = glob(directory + "/*")
-	changed = [os.path.relpath(path, directory) for path in items if storage.is_path_changed(path)]
+	items = [os.path.relpath(path, directory) for path in glob(directory + "/*")]
 
-	if len(changed) < 1:
+	if len(items) < 1:
 		print_progress_bar(1, 1, suffix = 'Complete!', length = 50)
 		return 0
 
@@ -49,10 +48,10 @@ def push(directory, cleanup=False):
 	src_root = directory.replace("\\", "/")
 
 	progress = 0
-	for filename in changed:
+	for filename in items:
 		src = src_root + "/" + filename
 		dst = dst_root + "/" + filename
-		print_progress_bar(progress, len(changed), suffix = f'Pushing {filename}' + (" " * 20), length = 50)
+		print_progress_bar(progress, len(items), suffix = f'Pushing {filename}' + (" " * 20), length = 50)
 		subprocess.call([make_config.get_adb(), "shell", "rm", "-r", dst], stderr=ignore, stdout=ignore)
 		result = subprocess.call([make_config.get_adb(), "push", src, dst], stderr=ignore, stdout=ignore)
 		progress += 1
@@ -61,7 +60,7 @@ def push(directory, cleanup=False):
 			print(f"failed to push to directory {dst_root} with code {result}")
 			return result
 	
-	print_progress_bar(progress, len(changed), suffix = f'Complete!' + (" " * 20), length = 50)
+	print_progress_bar(progress, len(items), suffix = f'Complete!' + (" " * 20), length = 50)
 	storage.save()
 	return result
 
