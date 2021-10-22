f17ca3594fcce66e9785aba50c17f10677fea7bb
vsdum <danmarkhunter13072006@gmail.com>
2021-10-22 23:00:38 +0300

    Added version check to launcher script

--- src/launcher.js
+++ src/launcher.js
@@ -1,6 +1,10 @@
-ConfigureMultiplayer({
-	name: "Fast Leaf Decay",
-	version: "1.0",
-	isClientOnly: false
-});
-Launch();
+const json = FileTools.ReadJSON(__packdir__ + "manifest.json");
+const version = parseInt(json.gameVersion.split(".")[1])
+if(version >= 16) {
+	ConfigureMultiplayer({
+		name: "Fast Leaf Decay",
+		version: "1.0",
+		isClientOnly: false
+	});
+	Launch();
+} else throw new Error("Fast Leaf Decay mod only works on Minecraft BE 1.16.201 and above (in future). You are currently playing on 1." + version + ".x version! If you are using InnerCore Legacy pack, consider moving to the main one, if you want to use Fast Leaf Decay mod!");
